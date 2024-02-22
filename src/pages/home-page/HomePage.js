0cbac4830ed017e7d404add7d4a3a26cb12f6662
Elena5585 <120042764+Elena5585@users.noreply.github.com>
2024-02-22 13:28:30 +0500

    try 100vw

--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -225,8 +225,8 @@ export default function HomePage() {
               ))}              
             </div>
             <div className='trends__cards-arrows' style={searchedTrends.length < 2 ? styles.passiveStyle : styles.activeStyle}>
-                <div className='trends__cards-arrow--left' style={styles.headerText} disabled={!canScrollLeft} onClick={() => scrollContainerBy(-400)}>←</div>                
-                <div className='trends__cards-arrow--right' style={styles.headerText} disabled={!canScrollRight} onClick={() => scrollContainerBy('100vw')}>→</div>
+                <div className='trends__cards-arrow--left' style={styles.headerText} disabled={!canScrollLeft} onClick={() => scrollContainerBy(-500)}>←</div>                
+                <div className='trends__cards-arrow--right' style={styles.headerText} disabled={!canScrollRight} onClick={() => scrollContainerBy(500)}>→</div>
             </div>            
           </div>        
         </section>
